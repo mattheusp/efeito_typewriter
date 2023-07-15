59fc63d4270ba81167eab6caca1dd8fe7031809c
Mattheus <mattheusp382@gmail.com>
2023-07-15 16:00:39 -0300

    commit_v2

--- scripts.js
+++ scripts.js
@@ -2,12 +2,10 @@ function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        console.log(i);
-        setTimeout(function() {
-            
-        }, )
-    })
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
 }
 
 const titulo = document.querySelector('h1');
 typeWriter(titulo);
+
